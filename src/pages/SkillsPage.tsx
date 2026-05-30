56d8c482a9f9ebf60f82f468baa0372f5ff06b44
Your Name <vbudiongan.student@asiancollege.edu.ph>
2026-05-30 16:22:09 +0800

    akhfajhgjg

---
+++ src/pages/SkillsPage.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
+import Footer from "../components/Footer";
+
+const SkillsPage = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Skills />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default SkillsPage;
+
